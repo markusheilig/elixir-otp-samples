029fde4d999631dc8917b94ffc9d5695b98598cb
Markus Heilig <markusheilig@gmx.net>
2017-10-05 12:07:53 +0200

    refactor test

--- pooly/test/worker_supervisor_test.exs
+++ pooly/test/worker_supervisor_test.exs
@@ -1,5 +1,6 @@
 defmodule WorkerSupervisorTest do
   use ExUnit.Case
+
   alias Pooly.{WorkerSupervisor, SampleWorker}
 
   test "children are restarted automatically" do
@@ -7,15 +8,12 @@ defmodule WorkerSupervisorTest do
     {:ok, _} = Supervisor.start_child(sup, [[]])
     {:ok, _} = Supervisor.start_child(sup, [[]])
     {:ok, worker_pid} = Supervisor.start_child(sup, [[]])
-    SampleWorker.stop(worker_pid)
+    :ok = SampleWorker.stop(worker_pid)
     %{workers: count} = Supervisor.count_children(sup)
     assert count == 3
-    pids = Supervisor.which_children(sup) |> Enum.map(fn {_,pid,_,_} -> pid end)
-    assert pids |> contains(worker_pid) == false
+    worker_pids =
+      Supervisor.which_children(sup)
+      |> Enum.map(fn {_, pid, _, _} -> pid end)
+    assert worker_pid not in worker_pids
   end
-
-  def contains([], _), do: false
-  def contains([h|_], h), do: true
-  def contains([_|t], x), do: contains(t, x)
-
 end
