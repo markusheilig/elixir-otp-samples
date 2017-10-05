2d072c717f05dccfb93a97fefc2b5d0e57905887
Markus Heilig <markusheilig@gmx.net>
2017-10-05 12:07:30 +0200

    format code

--- pooly/lib/pooly/worker_supervisor.ex
+++ pooly/lib/pooly/worker_supervisor.ex
@@ -2,29 +2,22 @@ defmodule Pooly.WorkerSupervisor do
   use Supervisor
 
   # API
-
-  def start_link({_,_,_} = mfa)do
-    Supervisor.start_link(__MODULE__, mfa)
+  def start_link({_, _, _} = mfa) do
+    Supervisor.start_link __MODULE__, mfa
   end
 
-  # Callbacks
 
+  # Callbacks
   def init({m, f, a}) do
     # :permanent = worker is always to be restarted
     # f = function to start the worker
-    worker_opts = [restart: :permanent,
-                   function: f]
+    worker_opts = [restart: :permanent, function: f]
     # create a list of child processes
     children = [worker(m, a, worker_opts)]
     # supervisor options, give up if there are more than
     # 5 restarts within 5 seconds
-    opts = [strategy: :simple_one_for_one,
-            max_restarts: 5,
-            max_seconds: 5]
+    opts = [strategy: :simple_one_for_one, max_restarts: 5, max_seconds: 5]
     # create child specification
-    supervise(children, opts)
+    supervise children, opts
   end
-
-
-
 end
